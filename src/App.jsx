b59b16f2347eae78713b22ccfdf6796cdfbc0289
rodrigomones <rodrigomones@gmail.com>
2021-06-03 11:46:51 -0300

    install packages

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          mones-gallery-shop
+          mones-rodrigo
         </a>
       </header>
     </div>
