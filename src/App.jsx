24c3dc63b4864a04193fde4e6b73a27b1224e45f
rodrigomones <rodrigomones@gmail.com>
2021-06-10 10:09:49 -0300

    Add-ItemListContainer-clase4-Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { ItemListContainer } from "./components/ItemListContainer";
 import { NavBar } from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer />
     </div>
   );
 }
---
+++ src/components/ItemListContainer/index.jsx
@@ -0,0 +1,9 @@
+import { Typography } from "@material-ui/core";
+
+export const ItemListContainer = () => {
+  return (
+    <>
+      <Typography variant="h1">Hello World!</Typography>
+    </>
+  );
+};
